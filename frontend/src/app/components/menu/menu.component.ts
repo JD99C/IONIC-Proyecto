9ab83a1143fd9535c89b53575b0e06f0b5a7514a
Juan de Dios <jdmillares123@gmail.com>
2022-10-06 16:04:57 +0100

    Lo que falta
    Backend completo , Paginas Lista Actualizar y Crear

--- frontend/src/app/components/menu/menu.component.ts
+++ frontend/src/app/components/menu/menu.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-menu',
@@ -6,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.scss'],
 })
 export class MenuComponent implements OnInit {
+  
 
-  constructor() { }
+  constructor(private alertController: AlertController) { 
+    ;
+  }
+
+  imagen = "../../../assets/imagen/AllMight2.png"
 
   ngOnInit() {}
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Información sobre el Creador',
+      subHeader: '',
+      message: `<img src="${this.imagen}" alt="g-maps" style="border-radius: 2px"> ¡Hola!, Esta es mi aplicación sobre coches, la verdad es que no tuve ninguna inspiración cuando decidí hacerla sobre esto, pero algo tenia que hacer y esto es lo que se me ocurrió. ¡Espero que os guste! :D ` ,
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
+
+
 
 }
